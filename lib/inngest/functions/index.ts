f36bcfcc3aba67cc133f6cf3095f7855e66f4dfa
Blessan-Corley <blessancorley@gmail.com>
2026-05-02 17:19:00 +0530

    refactor: update inngest functions barrel index

--- lib/inngest/functions/index.ts
+++ lib/inngest/functions/index.ts
@@ -5,8 +5,10 @@ import { onJobPosted } from './onJobPosted';
 import { onBulkNotificationSend, onNotificationSend } from './onNotificationSend';
 import { onOrphanUpload } from './onOrphanUpload';
 import { onPaymentConfirmed } from './onPaymentConfirmed';
+import { onPaymentFailed } from './onPaymentFailed';
 import { onUserSignup } from './onUserSignup';
 import { closeInactiveJobs } from './scheduled/closeInactiveJobs';
+import { expireReviews } from './scheduled/expireReviews';
 import { orphanUploadSweep } from './scheduled/orphanUploadSweep';
 
 export const inngestFunctions = [
@@ -16,10 +18,12 @@ export const inngestFunctions = [
   onApplicationAccepted,
   onApplicationRejected,
   onPaymentConfirmed,
+  onPaymentFailed,
   onDisputeOpened,
   onNotificationSend,
   onBulkNotificationSend,
   onOrphanUpload,
   closeInactiveJobs,
+  expireReviews,
   orphanUploadSweep,
 ];
